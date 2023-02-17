104dd91ac3d22c8059d210c05f99bbf3cad8d038
Mesa205 <sebasmesar7@gmail.com>
2023-02-17 18:22:44 -0500

    first commit

--- Notes/Front/vite-project/src/hooks/userSocket.jsx
+++
@@ -1,8 +0,0 @@
-import { useMemo } from "react";
-import { io } from "socket.io-client"
-
-export const useSocket=(server)=>{
-    const socket = useMemo(()=>io(server,{transports:["websocket"]}),[server]);
-
-    return { socket };
-};
